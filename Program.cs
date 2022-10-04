76f6640b88e661244997e147efbf613f4d07714b
baileydiaz <baileydiaz10@gmail.com>
2022-10-04 13:33:35 -0600

    hello

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+for loop
+    call score class
+    call deck class
+    pull card from deck with index
+    Console.Write($"The card is {}");
+
+    Console.Write("Higher or Lower");
+    string user_guess = Console.ReadLine();
+
+    
+    Console.Write($"Next card was: {}")
--- classes.cs
+++ classes.cs
@@ -9,5 +9,17 @@ class Score
 
 class Deck 
 {
+    private static Random rng = new Random();  
 
+public static void Shuffle<T>(this IList<T> list)  
+{  
+    int n = list.Count;  
+    while (n > 1) {  
+        n--;  
+        int k = rng.Next(n + 1);  
+        T value = list[k];  
+        list[k] = list[n];  
+        list[n] = value;  
+    }  
+}
 }
