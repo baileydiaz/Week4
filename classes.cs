ad214ff282eb65ff393bbd13251ae526d911134a
baileydiaz <baileydiaz10@gmail.com>
2022-10-07 17:34:18 -0600

    pls

--- classes.cs
+++ classes.cs
@@ -1,10 +1,32 @@
 class Score
 {
-    public int score_creator()
-    {
-        int intital_score = 300;
-        return intital_score;
+    int initial_score = 300;
+    public Score(){
     }
+
+    public int calculate_score(int current_score, string user_guess, int old_card, int new_card){
+    
+        if (new_card == old_card){
+           return current_score;
+        }   
+
+        else if (user_guess == "h"){
+            if(new_card > old_card){
+                return current_score += 100;
+                }
+                else{
+                    return current_score -= 75;
+            }}
+        
+
+        else{
+            if(new_card < old_card){
+                return current_score += 100;}
+                else{
+                    return current_score -= 75;
+                }
+            }
+        }
 }
 
-
+// 
