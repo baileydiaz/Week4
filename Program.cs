fac9b971091efba0de427eb534d2bf17ead01438
baileydiaz <baileydiaz10@gmail.com>
2022-10-07 09:39:13 -0600

    made changes to the original file

--- Program.cs
+++ Program.cs
@@ -1,24 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-while (x = 'yes') and (score > 0 ){
-
-    score = Score()
-    call score class
-
-    call deck class
-    deck = deck()
-    pull card from deck with index
-    Console.Write($"The card is {}");
-
-    Console.Write("Higher or Lower");
-    string user_guess = Console.ReadLine();
-
-    pull card from deck
-    Console.Write($"Next card was: {}");
-
-    need to calculate score
-    Console.Write($"You score is: {}");
-
-    Console.Write("[y/n]");
+﻿// See https://aka.ms/new-console-template for more information 
 
 
+class Program{
+    static void Main(string[] args){
+        string x = "yes";
+        int score = 1;
+        while ((score > 0) && (x == "yes")){
+
+            Score running_score = new Score();
+
+            getDeck deckOfCards = new getDeck();
+
+            pull card from deck with index
+            Console.Write($"The card is {}");
+
+            Console.Write("Higher or Lower");
+            string user_guess = Console.ReadLine();
+
+            pull card from deck
+            Console.Write($"Next card was: {}");
+
+            need to calculate score
+            Console.Write($"You score is: {}");
+
+            Console.Write("[y/n]");
+        }
+    }     
+    static pull_random_card{
+        
+    }   
+
+    static calculate_score{
+
+    }
 }
--- classes.cs
+++ classes.cs
@@ -1,25 +1,10 @@
 class Score
 {
-    public int score()
+    public int score_creator()
     {
         int intital_score = 300;
         return intital_score;
     }
 }
 
-class Deck 
-{
-    private static Random rng = new Random();  
 
-public static void Shuffle<T>(this IList<T> list)  
-{  
-    int n = list.Count;  
-    while (n > 1) {  
-        n--;  
-        int k = rng.Next(n + 1);  
-        T value = list[k];  
-        list[k] = list[n];  
-        list[n] = value;  
-    }  
-}
-}
