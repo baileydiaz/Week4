1605e5fc9d90be77041e034745db594d8c170e77
Justin Ackerman <justin.d.ackerman@gmail.com>
2022-10-07 12:24:10 -0600

    pull random card

--- Deck.cs
+++ Deck.cs
@@ -2,9 +2,16 @@ class Deck {
 
     List<int> GetNumbers(int start, int end)
     {
-        List<int> list = new List<int>();
+        List<int> Cards = new List<int>();
         for (int i = start; i < end; i++)
-            list.Add(i);
-        return list;
+            Cards.Add(i);
+        return Cards;
     }
+    Random generator = new Random();
+public int pull_random_card(List<int> Cards){ 
+        int randIndex = generator.Next(Cards.Count);
+        int card_used = Cards[randIndex];
+        Cards.RemoveAt(randIndex);
+        return card_used;
+    }  
 }
